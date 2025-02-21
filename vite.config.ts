077c26001285a8cebd450a46caee049669b7fae5
Zhiheng Zhang <55773069+zhihengGet@users.noreply.github.com>
2025-02-21 14:44:03 -0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }),sveltekit()]
+});
